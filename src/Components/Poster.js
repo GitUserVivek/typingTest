8f89643ff03d79236a837380418cd95c969be08c
Jvs <jagtapvs9832@gmail.com>
2021-10-10 16:07:35 +0530

    FrontEnd completed

---
+++ src/Components/Poster.js
@@ -0,0 +1,8 @@
+import '../index.css';
+
+export default function Poster(props) {
+
+    return <div className='poster'>
+        {props.classname}
+    </div>;
+}
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,9 +1,27 @@
-import react from 'react';
+import Poster from './Components/Poster';
+import Board from './Components/Board';
+import './index.css';
 function HomePage() {
   return <>
     <div>
-      <div className="Container">
+      <div className="container">
+        <div className="topBanner"></div>
+        <div className="mainBody">
+          <strong>  Lets Check Your Typing Test</strong>
+          <div className="postersRow">
+            <Poster classname="Timer" />
+            <Poster classname="Correct" />
+            <Poster classname="Wrong" />
+          </div>
+          <Board />
 
+          {/* Input box started..  */}
+          <div className="inputField">
+            <div className="Words">ff</div>
+            <input type="text" autoCorrect={false} spellCheck="false" ></input>
+          </div>
+
+        </div>
       </div>
     </div>
 
